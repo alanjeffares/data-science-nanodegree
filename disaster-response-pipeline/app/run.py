740ddf81832220cf7b85b4b7f8bc430e5014befa
alanjefares <alan.jeffares.1@ucdconnect.ie>
2019-10-21 19:41:53 +0100

    adding preprocessing functionality

--- disaster-response-pipeline/app/run.py
+++ disaster-response-pipeline/app/run.py
@@ -26,8 +26,8 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('tweets', engine)
 
 # load model
 model = joblib.load("../models/your_model_name.pkl")
--- disaster-response-pipeline/data/process_data.py
+++ disaster-response-pipeline/data/process_data.py
@@ -1,16 +1,34 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
+def extract_category(input_val: str) -> str:
+    category, _ = input_val.split('-')
+    return category
+
+def category_to_bool(input_val: str) -> int:
+    _, value = input_val.split('-')
+    return int(value)
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories)
+    return df
 
 def clean_data(df):
-    pass
-
+    categories = df['categories'].str.split(';', expand=True)
+    categories.columns = categories.iloc[0,:].map(extract_category).values
+    categories = categories.applymap(category_to_bool)
+    df = df.drop('categories', axis=1)
+    df = df.merge(categories, left_index=True, right_index=True)
+    df.drop_duplicates(inplace=True)
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    db_path = 'sqlite:///{}'.format(database_filename)
+    engine = create_engine(db_path)
+    df.to_sql('tweets', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -37,6 +55,7 @@ def main():
               'to as the third argument. \n\nExample: python process_data.py '\
               'disaster_messages.csv disaster_categories.csv '\
               'DisasterResponse.db')
+
 
 
 if __name__ == '__main__':
